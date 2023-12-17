954beb4c42b4081f7fe296122eccd23584d3165b
Kajiru <sdkfz181tiger@gmail.com>
2023-12-17 17:22:46 +0900

    Update

--- 2023/SukkiriML/sec05/test03/main.py
+++ 2023/SukkiriML/sec05/test03/main.py
@@ -25,7 +25,32 @@ def main():
 
 	# DataFrame
 	df = pd.read_csv(FILE_CSV)
+	
+	# 先頭の3件
 	print(df.head(3))
+	# 行数
+	print("行数:", df.shape[0])
+	# 列数
+	print("列数:", df.shape[1])
+
+	# target列にあるデータ種類
+	print("データ種類:", df["target"].unique())
+
+	# 各データがいくつあるか
+	print("x0:", df["x0"].size)
+	print("x1:", df["x1"].size)
+	print("x2:", df["x2"].size)
+	print("x3:", df["x3"].size)
+
+	# 各データに欠損数がいくつあるか
+	print("欠損数x0:", df["x0"].isnull().sum())
+	print("欠損数x1:", df["x1"].isnull().sum())
+	print("欠損数x2:", df["x2"].isnull().sum())
+	print("欠損数x3:", df["x3"].isnull().sum())
+
+	# 欠損がある場合、各列の中央値で埋める
+	
+
 	"""
 	# Fill lost data with average point
 	mean = df.mean(numeric_only=True)# 欠損値を除外して計算
