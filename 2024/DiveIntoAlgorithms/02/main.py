7094c1e967335e8f9223a232212b4c283e73f716
Kajiru <sdkfz181tiger@gmail.com>
2024-03-01 21:02:03 +0900

    Update

---
+++ 2024/DiveIntoAlgorithms/02/main.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+"""
+Dive Into Algorithms
+"""
+
+#==========
+# Main
+
+import matplotlib.pyplot as plt
+
+def main():
+	print("main!!")
+
+	# Title, Label
+	plt.title("The Trajectory of Thrown Ball")
+	plt.xlabel("Horizontal Position of Ball")
+	plt.ylabel("Vertical Position of Ball")
+
+	# 自由落下での放物線を描画(0.1m単位)
+	xs0 = [x/100 for x in list(range(201))]
+	ys0 = [trajectory(x) for x in xs0]
+
+	# 着地点からの視線(x座標が0.1)
+	xs1 = [0.1, 2]
+	ys1 = [trajectory(0.1), 0]
+
+	# 着地点からの視線(x座標が0.2)
+	xs2 = [0.2, 2]
+	ys2 = [trajectory(0.2), 0]
+
+	# 着地点からの視線(x座標が0.3)
+	xs3 = [0.3, 2]
+	ys3 = [trajectory(0.3), 0]
+
+	# 三角形の比
+	xs4 = [0.3, 0.3]
+	ys4 = [0, trajectory(0.3)]
+	xs5 = [0.3, 2]
+	ys5 = [0, 0]
+	plt.text(0.3, trajectory(0.3)/2, "A", fontsize=16)
+	plt.text((0.3+2)/2, 0.05, "B", fontsize=16)
+
+	plt.plot(xs0, ys0, xs1, ys1, xs2, ys2, xs3, ys3, xs4, ys4, xs5, ys5)
+	plt.show()
+
+# 自由落下での放物線の関数
+#	ここでは仮に、
+# 		v1: x方向の初速 -> 0.9
+# 		v2: y方向の初速 -> 10.0
+#   	a:  重力 -> 10
+#	として、次の2つの方程式から求める
+# 		x = v1t
+# 		y = v2t + (at^2)/2
+def trajectory(x):
+	return 10*x - 5*(x**2)
+
+if __name__ == "__main__":
+	main()
---
+++ 2024/DiveIntoAlgorithms/03/main.py
@@ -0,0 +1,81 @@
+# coding: utf-8
+
+"""
+チャップマンアルゴリズムによる弾道計算
+"""
+
+#==========
+# Main
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+	print("main!!")
+
+	# Title, Label
+	plt.title("The Trajectory of Thrown Ball")
+	plt.xlabel("Horizontal Position of Ball")
+	plt.ylabel("Vertical Position of Ball")
+
+	# 自由落下での放物線を描画(0.1m単位)
+	xs0 = [x/100 for x in list(range(201))]
+	ys0 = [trajectory(x) for x in xs0]
+
+	# 0.1秒間隔で点を打つ
+	xs1 = [pos_x(t/10) for t in list(range(5))]
+	ys1 = [pos_y(t/10) for t in list(range(5))]
+
+	# 野手の位置(ほぼ着地位置である)
+	xp = 1.99
+	yp = 0.0
+
+	# 0.1秒間隔でタンジェント値を求める
+	tans = [(y-yp)/(xp-x) for x, y in zip(xs1, ys1)]
+	#print(tans)
+
+	# タンジェント値の速度
+	diff_v = np.diff(tans)
+	#print(diff_v)
+
+	# タンジェント値の加速度
+	diff_a = np.diff(diff_v)
+	print(diff_a)
+
+	# 加速度が+なら後ろに下がり、-なら前に進む指示を出す
+	if 0.0 < diff_a[0]:
+		print("後ろに下がってください!!")
+	else:
+		print("前に進んでください!!")
+
+	#plt.plot(xs0, ys0, xs1, ys1)
+	#plt.show()
+
+# 自由落下での放物線の関数
+#	ここでは仮に、
+# 		vx: x方向の初速 -> 0.99
+# 		vy: y方向の初速 -> 9.9
+#   	gy: y方向の重力 -> -9.81
+#	として、次の2つの方程式から求める
+# 		x = vx*t
+# 		y = vy*t + (gy*t^2)/2
+
+vx = 0.99  # x方向の初速
+vy = 9.9   # y方向の初速
+gy = -9.81 # y方向の重力
+
+def trajectory(x):
+	return vy*(x/vx) + gy/2*(x/vx)**2
+
+def pos_x(t):
+	return vx*t
+
+def pos_y(t):
+	return vy*t + gy/2 * t**2
+
+def calc_v(x, y, t):
+	return math.sqrt(x**2 + y**2) / t
+
+if __name__ == "__main__":
+	main()
