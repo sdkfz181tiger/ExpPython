2a82058357d373c12a583fc1d937765bbcfef919
Kajiru <sdkfz181tiger@gmail.com>
2024-03-07 10:46:50 +0900

    Update

--- 2024/DiveIntoAlgorithms/06/main_2.py
+++ 2024/DiveIntoAlgorithms/06/main_2.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 """
-処理回数をグラフにする
+処理回数のグラフ
 """
 
 #==========
@@ -11,6 +11,9 @@ import math, random
 import matplotlib.pyplot as plt
 import numpy as np
 
+# やりすぎるとコードの可読性が下がるのでグローバルにしておく
+steps = 0# Steps
+
 def main():
 	print("main!!")
 
@@ -19,47 +22,45 @@ def main():
 	plt.xlabel("Total numbers")
 	plt.ylabel("Steps required to sort")
 
-	# Points
+	# Insertion Sort
 	xs = [i * 2 for i in range(50)]
-	ys = [test_sort(x) for x in xs]
-	plt.plot(xs, ys)
+	ys = [test_insertion(x) for x in xs]
+	plt.plot(xs, ys, "r-", label="Insertion Sort")
 
 	# 表示範囲を限定
 	plt.gca().set_ylim([np.min(ys), np.max(ys)])
 
 	# 指数関数的増加の曲線
-	ys_exp = [math.exp(x) for x in xs]
-	plt.plot(xs, ys_exp)
+	ys = [math.exp(x) for x in xs]
+	plt.plot(xs, ys, "g-", label="Exp")
 
 	# x^2の曲線
-	ys_squared = [x**2 for x in xs]
-	plt.plot(xs, ys_squared)
+	ys = [x**2 for x in xs]
+	plt.plot(xs, ys, "b-", label="Squared")
 
 	# x^3の曲線
-	ys_cubed = [x**3 for x in xs]
-	plt.plot(xs, ys_cubed)
+	ys = [x**3 for x in xs]
+	plt.plot(xs, ys, "c-", label="Cubed")
 
 	# x^1.5の曲線
-	ys_threehalves = [x**1.5 for x in xs]
-	plt.plot(xs, ys_threehalves)
+	ys = [x**1.5 for x in xs]
+	plt.plot(xs, ys, "m-", label="ThreeHalves")
 
+	plt.legend()
 	plt.show()
 
-def test_sort(size):
-	# Numbers
-	nums = [i for i in range(size)]
-	# Shuffle
-	random.seed(0)
-	random.shuffle(nums)
-	# Insertion sort
-	arr, steps = insertion_sort(nums)
-	#print("arr:", arr, " steps:", steps)
+def test_insertion(size):
+	global steps; steps = 0# Reset
+	if size <= 0: return 0# Important
+	nums = [i for i in range(size)]# Numbers
+	random.shuffle(nums)# Shuffle
+	insertion_sort(nums)# Insertion sort
 	return steps
 
-# 挿入ソート
+# Insertion Sort
 def insertion_sort(nums):
+	global steps
 	arr = []
-	steps = 0
 	while 0 < len(nums):
 		n = nums.pop(0)
 		i = 0
@@ -68,7 +69,6 @@ def insertion_sort(nums):
 			if n < arr[i]: break
 			i += 1
 		arr.insert(i, n)
-	return arr, steps
 
 if __name__ == "__main__":
 	main()
