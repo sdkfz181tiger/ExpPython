c4ba70ad4e3732daedc73915bbe474fafb8a6948
Kajiru <sdkfz181tiger@gmail.com>
2024-09-27 14:30:34 +0900

    Update

--- 2024/Python3_Recipis/11/11-3-1/main.py
+++ 2024/Python3_Recipis/11/11-3-1/main.py
@@ -7,4 +7,34 @@ shutilモジュール
 #==========
 # Main
 
+"""
+フォルダ構成
+./from
+　├ hoge.txt
+　├ fuga.txt
+　├ piyo.txt
+./to
+　├ 
+"""
+
 import shutil
+from pathlib import Path
+
+# ファイルをコピー
+shutil.copy("./from/hoge.txt", "./to/a.txt")
+#shutil.copy("./from/hoge.txt", "./to/")# ディレクトリを指定
+p = Path("./from/a.txt")
+print(p.exists())
+# True
+
+# ファイルのコピー(メタデータもコピーする)
+shutil.copy2("./from/fuga.txt", "./to/b.txt")
+#shutil.copy2("./from/fuga.txt", "./to/")# ディレクトリを指定
+p = Path("./from/b.txt")
+print(p.exists())
+# True
+
+# 指定ディレクトリを再起的にコピー
+ignore = shutil.ignore_patterns("*.csv", "*.json")
+shutil.copytree("./from", "./other",
+	            ignore=ignore, dirs_exist_ok=True)
