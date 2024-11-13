171fccb5269056bfa4bb45e6af70222cb109d25d
Kajiru <sdkfz181tiger@gmail.com>
2024-11-13 10:10:46 +0900

    Update

--- 2023/CircleCapture/04_capture/main.py
+++ 2023/CircleCapture/04_capture/main.py
@@ -1,9 +1,19 @@
 # coding: utf-8
 
 """
-1, 視線計測時、各動画を1本にした動画を使う事(目線が泳がなく)
-2, original.mp4のフレームレートを合わせる事
+1, Install
+	$ brew install ffmpeg
+2, Fix fps to 30fps
 	$ ffmpeg -i original.mp4 -r 30 original_fps30.mp4
+3, Ready venv
+	source your_location_of_venv_directory/bin/activate
+4, Install modules
+	python3 -m pip install opencv-python
+	python3 -m pip install ffmpeg-python
+	python3 -m pip install moviepy
+	python3 -m pip install matplotlib
+5, Run
+	python3 main.py
 """
 
 from modules.circle_capture import CircleCapture
@@ -13,20 +23,13 @@ def main():
 	print("main")
 
 	# Directory
-	dir_input = "../assets/team07_input/"
-	dir_output  = "team07_output"
+	dir_input = "../assets/movies/"
+	dir_output  = "test_output"
 
 	capture_list = [
-		{"path_movie": "cap_01.mov", "file_png": "cap_01.png", "file_json": "cap_01.json"},
-		{"path_movie": "cap_02.mov", "file_png": "cap_02.png", "file_json": "cap_02.json"},
-		{"path_movie": "cap_03.mov", "file_png": "cap_03.png", "file_json": "cap_03.json"},
-		{"path_movie": "cap_04.mov", "file_png": "cap_04.png", "file_json": "cap_04.json"},
-		{"path_movie": "cap_05.mov", "file_png": "cap_05.png", "file_json": "cap_05.json"},
-		{"path_movie": "cap_06.mov", "file_png": "cap_06.png", "file_json": "cap_06.json"},
-		{"path_movie": "cap_07.mov", "file_png": "cap_07.png", "file_json": "cap_07.json"},
-		{"path_movie": "cap_08.mov", "file_png": "cap_08.png", "file_json": "cap_08.json"},
-		{"path_movie": "cap_09.mov", "file_png": "cap_09.png", "file_json": "cap_09.json"},
-		{"path_movie": "cap_10.mov", "file_png": "cap_10.png", "file_json": "cap_10.json"},
+		{"path_movie": "cap_01.mp4", "file_png": "cap_01.png", "file_json": "cap_01.json"},
+		{"path_movie": "cap_02.mp4", "file_png": "cap_02.png", "file_json": "cap_02.json"},
+		{"path_movie": "cap_03.mp4", "file_png": "cap_03.png", "file_json": "cap_03.json"}
 	]
 	
 	for capture in capture_list:
@@ -38,20 +41,13 @@ def main():
 	render_list = [
 		{"file_json": "cap_01.json", "color": (255,   0,   0)},
 		{"file_json": "cap_02.json", "color": (  0, 255,   0)},
-		{"file_json": "cap_03.json", "color": (  0,   0, 255)},
-		{"file_json": "cap_04.json", "color": (255, 255,   0)},
-		{"file_json": "cap_05.json", "color": (  0, 255, 255)},
-		{"file_json": "cap_06.json", "color": (255,   0, 255)},
-		{"file_json": "cap_07.json", "color": (100,   0,   0)},
-		{"file_json": "cap_08.json", "color": (  0, 100,   0)},
-		{"file_json": "cap_09.json", "color": (  0,   0, 100)},
-		{"file_json": "cap_10.json", "color": (255, 255, 255)},
+		{"file_json": "cap_03.json", "color": (  0,   0, 255)}
 	]
 
 	# CircleRenderer
-	cRendrer = CircleRenderer(dir_input + "original_fps30.mov")# レンダー元映像
-	cRendrer.renderFrame(dir_output, "out_01.mov", render_list)# 視線データを描画
-	cRendrer.writeAudio(dir_output, dir_input + "original_fps30.mov", "audio.mp3", "out_01.mov", "comp_01.mov")# Audio
+	cRendrer = CircleRenderer(dir_input + "original.mp4")# レンダー元映像
+	cRendrer.renderFrame(dir_output, "out_01.mp4", render_list)# 視線データを描画
+	cRendrer.writeAudio(dir_output, dir_input + "original.mp4", "audio.mp3", "out_01.mp4", "comp_01.mp4")# Audio
 	
 if __name__ == "__main__":
 	main()
