4a46532c5dd5cb738c7df59a8defb5ef25e07aed
Kajiru <sdkfz181tiger@gmail.com>
2024-11-28 19:28:22 +0900

    Update

--- 2023/CircleCapture/05_capture/main.py
+++ 2023/CircleCapture/05_capture/main.py
@@ -16,6 +16,7 @@
 	python3 main.py
 """
 
+import os, shutil
 from modules.circle_capture import CircleCapture
 from modules.circle_renderer import CircleRenderer
 
@@ -23,21 +24,28 @@ def main():
 	print("main")
 
 	# Directory
-	dir_input = "../assets/movies/"
-	dir_output  = "test_output"
+	dir_input  = "../assets/movies/"
+	dir_output = "test_output"
 
+	original_input = "12_original_fps30.mp4"
+
+	# Clean
+	shutil.rmtree(dir_output)
+	os.mkdir(dir_output)
+
+	# Capture
 	capture_list = [
-		{"path_movie": "w_left/01/01_fps30.mp4",  "file_png": "cap_01.png", "file_json": "cap_01.json"},
-		{"path_movie": "w_left/02/01_fps30.mp4",  "file_png": "cap_02.png", "file_json": "cap_02.json"},
-		{"path_movie": "w_left/03/01_fps30.mp4",  "file_png": "cap_03.png", "file_json": "cap_03.json"},
-		{"path_movie": "w_left/04/01_fps30.mp4",  "file_png": "cap_04.png", "file_json": "cap_04.json"},
-		{"path_movie": "w_right/01/01_fps30.mp4", "file_png": "cap_01.png", "file_json": "cap_05.json"},
-		{"path_movie": "w_right/02/01_fps30.mp4", "file_png": "cap_02.png", "file_json": "cap_06.json"},
-		{"path_movie": "w_right/03/01_fps30.mp4", "file_png": "cap_03.png", "file_json": "cap_07.json"},
-		{"path_movie": "w_right/04/01_fps30.mp4", "file_png": "cap_04.png", "file_json": "cap_08.json"},
-		{"path_movie": "door/01/01_fps30.mp4",    "file_png": "cap_01.png", "file_json": "cap_09.json"},
-		{"path_movie": "door/02/01_fps30.mp4",    "file_png": "cap_02.png", "file_json": "cap_10.json"},
-		{"path_movie": "door/03/01_fps30.mp4",    "file_png": "cap_03.png", "file_json": "cap_11.json"},
+		{"path_movie": "w_left/01/12_fps30.mp4",  "file_png": "cap_01.png", "file_json": "cap_01.json"},
+		{"path_movie": "w_left/02/12_fps30.mp4",  "file_png": "cap_02.png", "file_json": "cap_02.json"},
+		{"path_movie": "w_left/03/12_fps30.mp4",  "file_png": "cap_03.png", "file_json": "cap_03.json"},
+		{"path_movie": "w_left/04/12_fps30.mp4",  "file_png": "cap_04.png", "file_json": "cap_04.json"},
+		{"path_movie": "w_right/01/12_fps30.mp4", "file_png": "cap_05.png", "file_json": "cap_05.json"},
+		{"path_movie": "w_right/02/12_fps30.mp4", "file_png": "cap_06.png", "file_json": "cap_06.json"},
+		{"path_movie": "w_right/03/12_fps30.mp4", "file_png": "cap_07.png", "file_json": "cap_07.json"},
+		{"path_movie": "w_right/04/12_fps30.mp4", "file_png": "cap_08.png", "file_json": "cap_08.json"},
+		{"path_movie": "door/01/12_fps30.mp4",    "file_png": "cap_09.png", "file_json": "cap_09.json"},
+		{"path_movie": "door/02/12_fps30.mp4",    "file_png": "cap_10.png", "file_json": "cap_10.json"},
+		{"path_movie": "door/03/12_fps30.mp4",    "file_png": "cap_11.png", "file_json": "cap_11.json"}
 	]
 	
 	for capture in capture_list:
@@ -46,6 +54,7 @@ def main():
 		cCapture.captureFrame(dir_output, capture["file_png"])# Capture
 		cCapture.dumpJson(dir_output, capture["file_json"])# Dump
 	
+	# Render
 	render_list = [
 		{"file_json": "cap_01.json", "color": (255,   0,   0)},
 		{"file_json": "cap_02.json", "color": (  0, 255,   0)},
@@ -61,9 +70,9 @@ def main():
 	]
 
 	# CircleRenderer
-	cRendrer = CircleRenderer(dir_input + "original_fps30.mp4")# レンダー元映像
+	cRendrer = CircleRenderer(dir_input + original_input)# レンダー元映像
 	cRendrer.renderFrame(dir_output, "out_01.mp4", render_list)# 視線データを描画
-	cRendrer.writeAudio(dir_output, dir_input + "original_fps30.mp4", "audio.mp3", "out_01.mp4", "comp_01.mp4")# Audio
+	cRendrer.writeAudio(dir_output, dir_input + original_input, "audio.mp3", "out_01.mp4", "comp_01.mp4")# Audio
 	
 if __name__ == "__main__":
 	main()
