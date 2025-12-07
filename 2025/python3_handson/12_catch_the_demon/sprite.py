076ab9a2e3e6b239ac645e2743abf446b9792cac
Kajiru <sdkfz181tiger@gmail.com>
2025-12-07 16:32:24 +0900

    Update

---
+++ 2025/python3_handson/12_catch_the_demon/sprite.py
@@ -0,0 +1,88 @@
+# coding: utf-8
+
+"""
+かじるプログラミング_tkinter
+"""
+
+import math
+import random
+import tkinter
+
+class Ball:
+
+    def __init__(self, cvs, x, y, r):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.vx = 0
+        self.vy = 0
+        self.dead = False
+        # Oval
+        self.oval = cvs.create_oval(x-r, y-r, x+r, y+r,
+                                    fill="gray", width=0)
+        # Image
+        choice = random.choice(("r", "g", "b"))
+        file = "images/demon_{}.png".format(choice)
+        self.photo = tkinter.PhotoImage(file=file)
+        self.resized = self.photo.zoom(3, 3)
+        self.image = cvs.create_image(x, y, image=self.resized)
+
+    def update(self, cvs):
+
+        # Move
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+        
+        # Draw
+        cvs.coords(self.oval,
+                   self.x - self.r, self.y - self.r,
+                   self.x + self.r, self.y + self.r)
+
+        cvs.coords(self.image, self.x, self.y)
+
+    def set_x(self, x):
+        self.x = x
+
+    def set_y(self, y):
+        self.y = y
+
+    def is_inside(self, x, y):
+        if x < self.x - self.r: return False
+        if self.x + self.r < x: return False
+        if y < self.y - self.r: return False
+        if self.y + self.r < y: return False
+        return True
+
+    def die(self, cvs):
+        self.dead = True
+        self.stop()
+        # Oval
+        cvs.itemconfig(self.oval, fill="red")
+
+    def is_dead(self):
+        return self.dead
+
+    def move(self, spd, deg):
+        radian = deg * math.pi / 180
+        self.vx = spd * math.cos(radian)
+        self.vy = spd * math.sin(radian)
+
+    def stop(self):
+        self.move(0, 0)
+
+class ImageLoader:
+
+    photos = {} # Cache
+
+    @staticmethod
+    def load_photo(file):
+        if file not in ImageLoader.photos:
+            ImageLoader.photos[file] = tkinter.PhotoImage(file=file)
+        photo = ImageLoader.photos[file]
+        return photo, photo.width(), photo.height()
+
+    @staticmethod
+    def load_image(cvs, file, x=0, y=0):
+        photo, w, h = ImageLoader.load_photo(file)
+        return cvs.create_image(x, y, image=photo), w, h
+        
