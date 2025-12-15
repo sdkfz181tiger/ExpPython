880998182a67f27b86b426cf53c50df41b71b2a3
Kajiru <sdkfz181tiger@gmail.com>
2025-12-15 11:12:20 +0900

    Update

--- 2025/python3_arcade/06/main.py
+++ 2025/python3_arcade/06/main.py
@@ -34,8 +34,6 @@ class GameView(arcade.View):
         self.player = sprite.Ninja("images/ninja_01.png",
                                    x=W/2, y=H/2,
                                    scale=SPRITE_SCALE)
-        self.player.load_animation("front", "images/ninja_{:02d}.png", 5)
-        self.player.change_animation("front")
 
         # Sprites
         self.sprites = arcade.SpriteList()
--- 2025/python3_arcade/06/sprite.py
+++ 2025/python3_arcade/06/sprite.py
@@ -8,52 +8,16 @@ import arcade
 import math
 import random
 
-class Shape:
-
-    def __init__(self, x, y, r):
-        self.x = x
-        self.y = y
-        self.r = r
-        self.vx = 0
-        self.vy = 0
-        self.dead = False
-
-    def update(self, delta_time):
-        # Update
-        self.x += self.vx * delta_time
-        self.y += self.vy * delta_time
-
-    def draw(self):
-        # Draw
-        arcade.draw_ellipse_filled(self.x, self.y,
-                                   self.r, self.r,
-                                   arcade.color.WHITE, 0.0)
-
-class Ninja(arcade.Sprite):
+class AnimSprite(arcade.Sprite):
 
     def __init__(self, filename, x, y, scale):
         super().__init__(filename, scale=scale)
-        self.center_x = x
-        self.center_y = y
-        self.vx = 0
-        self.vy = 0
-
         # Animation
         self.anim_interval = 4
         self.anim_counter = 0
         self.anim_index = 0
         self.anim_key = ""
         self.anims = {}
-
-    def set_position(self, x, y):
-        self.center_x = x
-        self.center_y = y
-
-    def update(self, delta_time):
-        """ Update """
-        self.center_x += self.vx * delta_time
-        self.center_y += self.vy * delta_time
-        self.update_animation() # Animation
 
     def update_animation(self):
         """ Animation """
@@ -80,3 +44,26 @@ class Ninja(arcade.Sprite):
         self.anim_counter = 0
         self.anim_index = 0
         self.anim_key = key
+
+class Ninja(AnimSprite):
+
+    def __init__(self, filename, x, y, scale):
+        super().__init__(filename, x, y, scale=scale)
+        self.center_x = x
+        self.center_y = y
+        self.vx = 0
+        self.vy = 0
+
+        # Animation
+        self.load_animation("front", "images/ninja_{:02d}.png", 5)
+        self.change_animation("front")
+
+    def set_position(self, x, y):
+        self.center_x = x
+        self.center_y = y
+
+    def update(self, delta_time):
+        """ Update """
+        self.center_x += self.vx * delta_time
+        self.center_y += self.vy * delta_time
+        self.update_animation() # Animation
