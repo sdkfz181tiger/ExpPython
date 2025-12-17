0d005cb15cface37d5cdcff0205ca75d5066e068
Kajiru <sdkfz181tiger@gmail.com>
2025-12-17 22:35:30 +0900

    Update

--- 2025/python3_arcade/02/main.py
+++ 2025/python3_arcade/02/main.py
@@ -7,33 +7,43 @@
 import arcade
 import math
 import random
+import sprite
+import utility
 
 W, H = 480, 320 # ゲーム画面の幅と高さ
 TITLE = "Hello, Arcade!!" # タイトル
+FONT_SIZE = 10 # フォントサイズ
+SPRITE_SCALE = 1 # スプライトの倍率
+PLAYER_SPEED = 90 # プレイヤーの速度
 
 class GameView(arcade.View):
 
     def __init__(self):
-        """ 初期化 """
         super().__init__()
+
         # 背景色
         self.background_color = arcade.color.PAYNE_GREY
 
+        # Player
+        self.players = arcade.SpriteList()
+        self.player = sprite.Player("images/ninja/front_01.png",
+                                    x=W/2, y=H/2)
+        self.players.append(self.player)
+
     def on_key_press(self, key, key_modifiers):
-        """ キーを押した時 """
         pass
 
     def on_key_release(self, key, key_modifiers):
-        """ キーを離した時 """
         pass
 
     def on_update(self, delta_time):
-        """ 更新処理 """
-        pass
+
+        # Update
+        self.players.update()
 
     def on_draw(self):
-        """ 描画処理 """
         self.clear() # Clear
+        self.players.draw() # Draw
 
 def main():
     """ メイン処理 """
