e55d04d2417cc6339a91934ab680ad078db4fefa
Kajiru <sdkfz181tiger@gmail.com>
2025-12-21 17:36:45 +0900

    Update

--- 2025/python3_arcade/physics/01/main.py
+++ 2025/python3_arcade/physics/01/main.py
@@ -6,29 +6,29 @@
 
 import arcade
 import random
-import src.sprite as sprite
-import src.title as title
-import src.utility as utility
+import sprite
 
-# Result
-class ResultView(arcade.View):
+# Game
+class GameView(arcade.View):
 
     def __init__(self, window):
         super().__init__()
         self.window = window
+        self.w = self.window.width
+        self.h = self.window.height
+
+        # 背景色
         self.background_color = arcade.color.PAYNE_GREY
 
         # Info
         self.msg_info = arcade.Text(
-            "RESULT: SPACE TO NEXT", 
-            window.width/2, window.height-20,
+            "GAME", 
+            self.w/2, self.h-20, 
             arcade.color.WHITE, 12,
             anchor_x="center", anchor_y="top")
 
     def on_key_press(self, key, key_modifiers):
-        # Space to Game
-        if key == arcade.key.SPACE: 
-            self.window.show_view(title.TitleView(self.window)) # TitleView
+        pass
 
     def on_update(self, delta_time):
         pass
@@ -36,3 +36,12 @@ class ResultView(arcade.View):
     def on_draw(self):
         self.clear() # Clear
         self.msg_info.draw()
+
+def main():
+    """ メイン処理 """
+    window = arcade.Window(480, 320, "Hello, Arcade!!")
+    window.show_view(GameView(window)) # GameView
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
