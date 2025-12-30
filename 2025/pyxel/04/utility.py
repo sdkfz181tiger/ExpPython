90eba68d6427a99e5b422dd852dfdfc2d0b29577
Kajiru <sdkfz181tiger@gmail.com>
2025-12-30 16:07:50 +0900

    Update

---
+++ 2025/pyxel/04/utility.py
@@ -0,0 +1,26 @@
+# coding: utf-8
+
+"""
+かじるプログラミング_arcade
+"""
+
+import pyxel
+
+# Statsクラス
+class Stats:
+
+    def __init__(self, w, h):
+        pass
+
+    def key_press(self, key):
+        pass
+
+    def update(self, delta_time):
+        pass
+
+    def draw(self):
+        pass
+
+
+
+
