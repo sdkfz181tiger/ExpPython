b119344f100ca9e3916a38ad49fc4075d5f3994f
Kajiru <sdkfz181tiger@gmail.com>
2026-01-03 08:30:14 +0900

    Update

---
+++ 2025/pyxel/12/sprite.py
@@ -0,0 +1,82 @@
+# coding: utf-8
+
+"""
+かじるプログラミング_pyxel
+"""
+
+import pyxel
+import math
+import random
+
+class BaseSprite:
+
+    def __init__(self, x, y, w=8, h=8):
+        """ コンストラクタ """
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.vx = 0
+        self.vy = 0
+
+    def update(self):
+        """ 更新処理 """
+        self.x += self.vx
+        self.y += self.vy
+
+    def draw(self):
+        """ 描画処理(派生クラスで実装) """
+        pass
+
+    def move(self, spd, deg):
+        """ 移動 """
+        rad = deg * math.pi / 180
+        self.vx = spd * math.cos(rad) # x方向の速度
+        self.vy = spd * math.sin(rad) # y方向の速度
+
+    def flip_x(self):
+        """ x方向反転 """
+        self.vx *= -1
+
+    def intersects(self, other):
+        """ 矩形同士の当たり判定(AABB) """
+        if other.x + other.w < self.x: return False
+        if self.x + self.w < other.x: return False
+        if other.y + other.h < self.y: return False
+        if self.y + self.h < other.y: return False
+        return True
+
+class ShipSprite(BaseSprite):
+
+    def __init__(self, x, y):
+        """ コンストラクタ """
+        super().__init__(x, y)
+
+    def draw(self):
+        """ 描画処理 """
+        pyxel.blt(self.x, self.y, 0, 
+            0, 0, 
+            self.w, self.h, 0) # Ship
+
+class AsteroidSprite(BaseSprite):
+
+    def __init__(self, x, y):
+        """ コンストラクタ """
+        super().__init__(x, y)
+        self.index = random.randint(2, 7) # 隕石画像
+
+    def draw(self):
+        """ 描画処理 """
+        pyxel.blt(self.x, self.y, 0, self.w*self.index, 0, 
+                self.w, self.h, 0) # Ship
+
+class BulletSprite(BaseSprite):
+
+    def __init__(self, x, y):
+        """ コンストラクタ """
+        super().__init__(x, y)
+        self.x += self.w / 2 - 1 # 中央に調整
+
+    def draw(self):
+        """ 描画処理 """
+        pyxel.rect(self.x, self.y, 2, 2, 12)
