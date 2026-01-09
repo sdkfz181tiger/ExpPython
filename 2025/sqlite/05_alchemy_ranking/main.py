3b9f1ef8545c8dd410799d3cad87d70afd1bfeba
Kajiru <sdkfz181tiger@gmail.com>
2026-01-09 23:57:30 +0900

    Update

--- 2025/sqlite/05_alchemy_ranking/main.py
+++ 2025/sqlite/05_alchemy_ranking/main.py
@@ -8,14 +8,13 @@ import random
 import util_db
 
 def main():
-    """ メイン処理 """
 
     # Member
     names = ["Alex", "Becky", "Christy", "David"]
     comments = ["Champion!!", "Nice!!", "Good!!", "OMG...!!"]
     
     # MyDB
-    my_db = util_db.MyDB("data.sqlite", "ranking")
+    my_db = util_db.MyDB("data.sqlite")
 
     # Create
     my_db.create_table()
@@ -24,13 +23,14 @@ def main():
     name = random.choice(names)
     comment = random.choice(comments)
     score = random.randint(30, 300)
-    my_db.insert_record(name, comment, score)
-    
+    result = my_db.insert_record(name, comment, score)
+    print("insert:", result)
+
     # Update
     #my_db.update_record(1, "Thomas", "Hi, I'm hacker!!", 777)
 
     # Delete
-    #my_db.delete_record(1)
+    #my_db.delete_record(4)
 
     # Read(One)
     #record = my_db.read_record(1)
@@ -41,6 +41,7 @@ def main():
     print("= Ranking =")
     for record in records:
         print("Record:", record)
+    
 
 if __name__ == "__main__":
     main()
---
+++ 2025/sqlite/05_alchemy_ranking/util_db.py
@@ -0,0 +1,115 @@
+# coding: utf-8
+
+"""
+1, Install
+    $ python3 -m pip install sqlalchemy==2.0.15
+"""
+
+import datetime, hashlib, os
+from sqlalchemy import *
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+
+# SQLAlchemy
+Base = declarative_base()
+
+class MyDB():
+
+    def __init__(self, path):
+        """ コンストラクタ """
+        self.dir_base = os.path.dirname(__file__)
+        self.db_path = "sqlite:///" + os.path.join(self.dir_base, path)
+        # Engine, Database
+        self.db_engine = create_engine(self.db_path, echo=True)
+        self.db_session = sessionmaker(bind=self.db_engine)
+        self.db = self.db_session()
+
+    def __del__(self):
+        """ デストラクタ """
+        print("Destructor")
+        self.db.close()
+
+    # CRUD(Create)
+    def create_table(self):
+        """ テーブルを作る """
+        Base.metadata.create_all(self.db_engine)
+
+    # CRUD(Insert)
+    def insert_record(self, name, comment, score):
+        """ データを追加する """
+        print("insert_record:", name, comment)
+        record = Record(name, comment, score, self.get_time())
+        self.db.add(record)
+        self.db.commit()
+        return record.uid
+
+    # CRUD(Read)
+    def read_records(self, limit=10):
+        """ データを読み込む(limit件数まで) """
+        print("read_records:", limit)
+        stmt = select(Record).order_by(Record.score.desc())
+        return self.db.scalars(stmt)
+
+    def read_record(self, uid):
+        """ データを読み込む(1件) """
+        print("read_record:", uid)
+        try:
+            stmt = select(Record).where(Record.uid == uid)
+            return self.db.scalars(stmt).one()
+        except NoResultFound:
+            return None
+
+    # CRUD(Update)
+    def update_record(self, uid, name, comment, score):
+        """ データを更新する(1件) """
+        print("update_record:", uid)
+        record = self.read_record(uid)
+        if record == None: return -1
+        record.name = name
+        record.comment = comment
+        record.score = score
+        record.time_stamp = self.get_time()
+        self.db.commit()
+        return record.uid
+
+    # CRUD(Delete)
+    def delete_record(self, uid):
+        """ データを削除する(1件) """
+        print("delete_record:", uid)
+        record = self.read_record(uid)
+        if record == None: return -1
+        self.db.delete(record)
+        self.db.commit()
+        return record.uid
+
+    # Time
+    def get_time(self):
+        """ 現在日時を取得する """
+        return datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+
+# Model
+class Record(Base):
+
+    # Table
+    __tablename__ = "ranking"
+    # ID
+    uid = Column(Integer, primary_key=True, autoincrement=True)
+    # Name
+    name = Column(String(12), server_default="noname")
+    # Comment
+    comment = Column(String(24), server_default="nocomment")
+    # Score
+    score = Column(Integer, server_default="0")
+    # Timestamp
+    time_stamp = Column(String(24), server_default="1970/01/01 00:00:00")
+
+    def __init__(self, name, comment, score, time_stamp):
+        self.name = name
+        self.comment = comment
+        self.score = score
+        self.time_stamp = time_stamp
+
+    def __str__(self):
+        return "uid:{0}, name:{1}, comment:{2}, score:{3}, time_stamp:{3}".format(
+            self.uid, self.name, self.comment, self.score, self.time_stamp)
