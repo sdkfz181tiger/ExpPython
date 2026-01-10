142da7e5e7811214101baf3e0dbce76c5797ecb0
Kajiru <sdkfz181tiger@gmail.com>
2026-01-10 11:20:33 +0900

    Update

--- 2025/sqlite/05_alchemy_ranking/main.py
+++ 2025/sqlite/05_alchemy_ranking/main.py
@@ -16,34 +16,31 @@ def main():
     # MyDB
     my_db = util_db.MyDB("data.sqlite")
 
-    # Create
+    # Create(テーブルを作成する)
     my_db.create_table()
 
-    # Insert(Random)
+    # Insert(ランダムに1件追加する)
     name = random.choice(names)
     comment = random.choice(comments)
     score = random.randint(30, 300)
     result = my_db.insert_record(name, comment, score)
     print("insert:", result)
 
-    # Update
+    # Update(ハッカーが現れいたずらする)
     #my_db.update_record(1, "Thomas", "Hi, I'm hacker!!", 777)
 
-    # Delete
+    # Delete(uidが4のデータを削除する)
     #my_db.delete_record(4)
 
-    # Read(One)
+    # Read(uidが1のデータを1件取得する)
     #record = my_db.read_record(1)
     #print("Record:", record)
 
-    # Read(Ranking)
+    # Read(スコアの上位5件を取得する)
     records = my_db.read_records(limit=5)
     print("= Ranking =")
     for record in records:
         print("Record:", record)
-
-    # Close
-    my_db.close()
     
 
 if __name__ == "__main__":
--- 2025/sqlite/05_alchemy_ranking/util_db.py
+++ 2025/sqlite/05_alchemy_ranking/util_db.py
@@ -5,11 +5,17 @@
     $ python3 -m pip install sqlalchemy==2.0.15
 """
 
-import datetime, hashlib, os
-from sqlalchemy import *
+import datetime
+import os
+from sqlalchemy import (
+    create_engine,
+    Column,
+    Integer,
+    select,
+    String
+)
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import Session, sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm import sessionmaker
 
 class MyDB():
 
@@ -17,72 +23,71 @@ class MyDB():
         """ コンストラクタ """
         self.dir_base = os.path.dirname(__file__)
         self.db_path = "sqlite:///" + os.path.join(self.dir_base, path)
-        # Engine, Database
+        # Engine, Session
         self.db_engine = create_engine(self.db_path, echo=True)
         self.db_session = sessionmaker(bind=self.db_engine)
-        self.db = self.db_session()
+        Base.metadata.create_all(self.db_engine)# テーブルを作る
 
     # CRUD(Create)
-    def create_table(self):
-        """ テーブルを作る """
-        Base.metadata.create_all(self.db_engine)
-
-    # CRUD(Insert)
     def insert_record(self, name, comment, score):
         """ データを追加する """
         print("insert_record:", name, comment)
-        record = Record(name, comment, score, self.get_time())
-        self.db.add(record)
-        self.db.commit()
-        return record.uid
+        # Record
+        record = Record(
+            name=name, 
+            comment=comment, 
+            score=score, 
+            time_stamp=self.get_time())
+        with self.db_session() as session:
+            session.add(record)
+            session.commit()
+            return record.uid
 
     # CRUD(Read)
     def read_records(self, limit=10):
         """ データを読み込む(limit件数まで) """
         print("read_records:", limit)
-        stmt = select(Record).order_by(Record.score.desc()).limit(limit)
-        return self.db.scalars(stmt).all()
+        with self.db_session() as session:
+            stmt = select(Record).order_by(Record.score.desc()).limit(limit)
+            return session.scalars(stmt).all()
 
     def read_record(self, uid):
         """ データを読み込む(1件) """
         print("read_record:", uid)
-        stmt = select(Record).where(Record.uid == uid)
-        return self.db.scalars(stmt).one_or_none()
+        with self.db_session() as session:
+            return session.get(Record, uid)
 
     # CRUD(Update)
     def update_record(self, uid, name, comment, score):
         """ データを更新する(1件) """
         print("update_record:", uid)
-        record = self.read_record(uid)
-        if record == None: return -1
-        record.name = name
-        record.comment = comment
-        record.score = score
-        record.time_stamp = self.get_time()
-        self.db.commit()
-        return record.uid
+        with self.db_session() as session:
+            record = session.get(Record, uid)
+            if record is None: return -1
+            record.name = name
+            record.comment = comment
+            record.score = score
+            record.time_stamp = self.get_time()
+            session.commit()
+            return record.uid
 
     # CRUD(Delete)
     def delete_record(self, uid):
         """ データを削除する(1件) """
         print("delete_record:", uid)
-        record = self.read_record(uid)
-        if record == None: return -1
-        self.db.delete(record)
-        self.db.commit()
-        return record.uid
-
-    # Close
-    def close(self):
-        """ データベースを閉じる """
-        self.db.close
+        with self.db_session() as session:
+            record = session.get(Record, uid)
+            if record is None: return -1
+            session.delete(record)
+            session.commit()
+            return record.uid
 
     # Time
     def get_time(self):
         """ 現在日時を取得する """
         return datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
 
-# Model
+# ORM
 Base = declarative_base()
 class Record(Base):
 
@@ -91,19 +96,13 @@ class Record(Base):
     # ID
     uid = Column(Integer, primary_key=True, autoincrement=True)
     # Name
-    name = Column(String(12), server_default="noname")
+    name = Column(String(12), nullable=False)
     # Comment
-    comment = Column(String(24), server_default="nocomment")
+    comment = Column(String(24), nullable=False)
     # Score
-    score = Column(Integer, server_default="0")
+    score = Column(Integer, nullable=False)
     # Timestamp
-    time_stamp = Column(String(24), default="1970/01/01 00:00:00")
-
-    def __init__(self, name, comment, score, time_stamp):
-        self.name = name
-        self.comment = comment
-        self.score = score
-        self.time_stamp = time_stamp
+    time_stamp = Column(String(24), nullable=False)
 
     def __str__(self):
         return "uid:{0}, name:{1}, comment:{2}, score:{3}, time_stamp:{4}".format(
