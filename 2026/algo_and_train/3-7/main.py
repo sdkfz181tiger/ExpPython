ced3fa7422101635fcca345d0f8c46176cc49287
Kajiru <sdkfz181tiger@gmail.com>
2026-03-11 16:56:28 +0900

    Update

--- 2026/algo_and_train/3-7/main.py
+++ 2026/algo_and_train/3-7/main.py
@@ -2,21 +2,23 @@
 
 """
 アルトレ第二版
-3.6: 配列の要素の値を別の配列の要素番号に使用する
+3.7: 二つの正の整数の最大公約数を求める
 ※プログラムの場合、配列の要素は0から始まる
 """
 
-def sum_array(a, b):
-    n = len(b)
-    x = 0
-    for i in range(0, n):
-        print("b[{}]:{}".format(i, b[i]))
-        x = x + a[b[i]-1]
-    return x
+def gcd(a, b):
+    m = a
+    n = b
+    while m != n: # <- α (5回実行される)
+        if n < m:
+            m = m - n
+        else:
+            n = n - m
+    return m
 
 def main():
     """ メイン処理 """
-    print("Test:", sum_array([3,8,5,4,16,13,7,9,6,5], [3,5,8])) # 30
+    print("Test:", gcd(98, 42))
 
 if __name__ == "__main__":
     main()
