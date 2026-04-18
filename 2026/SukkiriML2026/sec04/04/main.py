6483ae18d68f051acca5584154a8e19e9542ed71
Kajiru <sdkfz181tiger@gmail.com>
2026-04-18 14:39:45 +0900

    Update

--- 2026/SukkiriML2026/sec04/04/main.py
+++ 2026/SukkiriML2026/sec04/04/main.py
@@ -10,15 +10,51 @@ import pandas as pd
 import pickle
 from sklearn import tree
 
-FILE_CSV = "./data.csv"
-FILE_PKL = "./data.pkl"
-COLUMNS_X = ["cm", "kg", "era"]
-COLUMNS_T = "group"
+COL_X    = ["japanese", "math", "english", "science"]
+COL_T    = "social"
+MY_CSV   = "my_data.csv"
+MY_MODEL = "my_model.pkl"
 
 def main():
 	""" Main """
 	print("main!!")
+
+	fit_model()
 	
+
+def fit_model():
+	""" Fit """
+	global COL_X, COL_T
+	print("fit_model")
+	
+	# CSV
+	df = pd.read_csv(MY_CSV)
+	print(df.head(3))
+	print(df.tail(3))
+
+	# 特定の列のみ
+	print(df["japanese"].head(3))
+	print(df[["japanese", "math"]].head(3))
+	"""
+	# 特徴量を準備
+	x = df[COL_X]
+	print(x.head(3))
+	print(type(x)) # DataFrame(複数列の場合)
+
+	# 教師データを準備
+	t = df[COL_T]
+	print(t.head(3))
+	print(type(t)) # Series(1列の場合)
+
+	# モデルを準備し、学習を実行
+	model = tree.DecisionTreeClassifier(random_state=0)
+	model.fit(x, t)
+
+	# モデルを評価
+	print("正解率:", model.score(x, t))
+
+	return model
+	"""
 	
 
 if __name__ == "__main__":
