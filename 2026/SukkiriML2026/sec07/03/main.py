15cc98f7ef39f5126392dee61d6c1f281844846c
Kajiru <sdkfz181tiger@gmail.com>
2026-04-20 18:32:32 +0900

    Update

--- 2026/SukkiriML2026/sec07/03/main.py
+++ 2026/SukkiriML2026/sec07/03/main.py
@@ -32,9 +32,17 @@ def main():
 	#    データの前処理を見直す
 	#    学習時の設定を変える
 	#    分析手法から見直す
-	for i in range(3, 10):
-		model, score_train, score_test = fit_model(depth=i)
-		print("深さ:", i, "訓練データ:", score_train, "検証データ:", score_test)
+
+	# for i in range(3, 10):
+	# 	model, score_train, score_test = fit_model(depth=i)
+	# 	print("深さ:", i, "訓練データ:", score_train, "検証データ:", score_test)
+
+	# 深さ7程度が適当である
+	model, score_train, score_test = fit_model(depth=7)
+
+	# 特徴重要度
+	result = pd.DataFrame(model.feature_importances_, index=COL_X)
+	print(result)
 	
 
 def fit_model(depth):
@@ -46,51 +54,6 @@ def fit_model(depth):
 	#print(df.head(3))
 	#print(df.tail(3))
 
-	# 教師データの種類をカウント
-	# 不均衡データ(種類の個数にバラツキがある)だとわかる
-	# 決定木モデルにとって、不均衡すぎると悪影響が出る
-	# 0: 549, 1: 342
-	#print(df["survived"].value_counts())
-
-	# 欠損値の総数
-	#print(df.isnull().sum())
-	
-	# 欠損値を穴埋め
-	# 穴埋めを何で行うかを検討
-	# print("Ageの平均値:", df["age"].mean())
-	# print("Ageの中央値:", df["age"].median())
-	# print("Ageの最頻値:", df["age"].mode()[0])
-	# df["age"] = df["age"].fillna(df["age"].mean()) # 平均値
-	# df["embarked"] = df["embarked"].fillna(df["embarked"].mode()[0]) # 最頻値
-
-	# グループ集計
-	print("survivedごとの平均年齢:")
-	print(df.groupby("survived")["age"].mean())
-
-	print("sexごとの平均年齢:")
-	print(df.groupby("sex")["age"].mean())
-
-	print("pclassごとの平均年齢:")
-	print(df.groupby("pclass")["age"].mean())
-
-	# クロス集計
-	#    デフォルトで平均値が集計される
-	print("行をsurvive, 列をpclassとして平均値で集計")
-	print(pd.pivot_table(df, 
-		index="survived", # 行
-		columns="pclass", # 列
-		values="age")) # 集計対象
-
-	print("中央値で集計")
-	print(pd.pivot_table(df, 
-		index="survived", # 行
-		columns="pclass", # 列
-		values="age", # 集計対象
-		aggfunc="median")) # 中央値
-
-	# 集計を元に検討し、欠損値を補完
-	print("欠損値の個数(Before):", df["age"].isnull().sum())
-
 	# 欠損している行かどうか(補完対象行かどうか)
 	is_null = df["age"].isnull()
 
@@ -98,19 +61,23 @@ def fit_model(depth):
 	df.loc[(df["pclass"]==1) & (df["survived"]==0) & is_null, "age"] = 43
 	df.loc[(df["pclass"]==1) & (df["survived"]==1) & is_null, "age"] = 35
 
-	print("欠損値の個数(After):", df["age"].isnull().sum())
-
 	# pclass=2に関する埋め込み(andは使えないので注意!!)
 	df.loc[(df["pclass"]==2) & (df["survived"]==0) & is_null, "age"] = 33
 	df.loc[(df["pclass"]==2) & (df["survived"]==1) & is_null, "age"] = 25
-
-	print("欠損値の個数(After):", df["age"].isnull().sum())
 
 	# pclass=2に関する埋め込み(andは使えないので注意!!)
 	df.loc[(df["pclass"]==3) & (df["survived"]==0) & is_null, "age"] = 26
 	df.loc[(df["pclass"]==3) & (df["survived"]==1) & is_null, "age"] = 20
 
-	print("欠損値の個数(After):", df["age"].isnull().sum())
+	#print("欠損値の個数(After):", df["age"].isnull().sum())
+
+	# ダミー変数化(ワンホットエンコーディング)
+	# sex列は、"male"と、"female"の文字列となっている為、これらをダミー変数とする
+	#    drop_first=Trueで、1つ減らした1列のデータフレームとして取得される
+	#    drop_first=Falseで、減らさず2列のデータフレームとして取得される
+	male = pd.get_dummies(df["sex"], drop_first=True, dtype=int)
+	#print(male)
+	df = pd.concat([df, male], axis=1) # データフレームに連結(列方向)
 
 	# ホールドアウト法で、訓練用と検証用とにデータを分割する
 	# test_size: 検証用データの割合
