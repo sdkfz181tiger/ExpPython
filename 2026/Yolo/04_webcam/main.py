8e16c6c6d939199e3b428ba7b29c0a619bca9179
Kajiru <sdkfz181tiger@gmail.com>
2026-07-03 17:52:06 +0900

    Update

--- 2026/Yolo/04_webcam/main.py
+++ 2026/Yolo/04_webcam/main.py
@@ -23,6 +23,9 @@ camera = cv2.VideoCapture(0, cv2.CAP_AVFOUNDATION)
 w = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 h = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+# Model
+model = YOLO("yolo26n.pt")
+
 
 def main():
     """ Main """
@@ -33,7 +36,7 @@ def main():
 
 
 def generate_frames():
-    global w, h
+    global w, h, model
 
     while True:
         # Camera
@@ -41,11 +44,15 @@ def generate_frames():
         if not success:
             break
 
+        # Track
+        frame_tracked = model.track(
+            frame, persist=True, verbose=False)[0].plot()
+
         # Grid
-        utility.draw_grid(w, h, frame, (255, 255, 255), 1)
+        utility.draw_grid(w, h, frame_tracked, (255, 255, 255), 1)
 
         # Frame -> JPG
-        ret, buffer = cv2.imencode(".jpg", frame)
+        ret, buffer = cv2.imencode(".jpg", frame_tracked)
 
         # JPG -> MIME
         yield(b"--frame\r\n"
@@ -64,53 +71,6 @@ def video_streaming():
     # Streaming
     return Response(generate_frames(), 
         mimetype="multipart/x-mixed-replace; boundary=frame")
-
-
-
-def start_prediction(path_file):
-    print("start_prediction:", path_file)
-
-    path = Path(path_file)
-    path_from = path
-    path_to   = path.name
-    path_comp = Path(f"{path.stem}_comp{path.suffix}")
-    
-    # Video(From)
-    cap_from   = cv2.VideoCapture(path_from)
-    w          = int(cap_from.get(cv2.CAP_PROP_FRAME_WIDTH))# Width
-    h          = int(cap_from.get(cv2.CAP_PROP_FRAME_HEIGHT))# Height
-    count      = int(cap_from.get(cv2.CAP_PROP_FRAME_COUNT))# Count
-    fps        = int(cap_from.get(cv2.CAP_PROP_FPS))# Fps
-    resolution = (w, h)
-    print("Video W:%d H:%d COUNT:%d FPS:%d" % (w, h, count, fps))
-
-    # Video(To)
-    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    cap_to = cv2.VideoWriter(
-        path_to, fourcc, fps, resolution)
-
-    # Model
-    model = YOLO("yolo26n.pt")
-
-    # Render
-    for n in range(count):
-        ret, frame = cap_from.read()# Read
-        if ret==False: break
-
-        # Track
-        frame_tracked = model.track(
-            frame, persist=True, verbose=False)[0].plot()
-
-        # Grid
-        utility.draw_grid(w, h, frame_tracked, (255, 255, 255), 1)
-        cap_to.write(frame_tracked)
-
-    # Release
-    cap_from.release()
-    cap_to.release()
-    
-    # Audio
-    #utility.write_audio(path_from, path_to, path_comp)
 
 
 if __name__ == "__main__":
