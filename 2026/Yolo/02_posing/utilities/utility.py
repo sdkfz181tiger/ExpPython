2ed09e602cf6c99bb6c029bfbe458d4cb9c14142
Kajiru <sdkfz181tiger@gmail.com>
2026-07-03 17:38:43 +0900

    Update

--- 2026/Yolo/02_posing/utilities/utility.py
+++ 2026/Yolo/02_posing/utilities/utility.py
@@ -7,16 +7,23 @@ from PIL import Image
 from ultralytics import YOLO, solutions
 
 
-def draw_grid(w, h, frame, l_color, l_width):
+def draw_grid(w, h, frame, l_color, l_width,
+    f_scale=1.0, f_color=(255, 255, 255), f_thickness=4):
     g_size = int(w / 20)
     rows = int(h / g_size)
     cols = int(w / g_size)
     for r in range(1, rows):
         y = r * g_size
         cv2.line(frame, (0, y), (w, y), l_color, l_width)
+        cv2.putText(frame, str(y), (10, y), 
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            f_scale, f_color, f_thickness)
     for c in range(1, cols):
         x = c * g_size
         cv2.line(frame, (x, 0), (x, h), l_color, l_width)
+        cv2.putText(frame, str(x), (x, h-10), 
+            cv2.FONT_HERSHEY_SIMPLEX, 
+            f_scale, f_color, f_thickness)
 
 
 def write_audio(path_from, path_to, path_comp):
