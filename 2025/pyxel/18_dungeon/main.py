7fc99aaaceb2d79b3d9d8bad1e1ad994d47cd1fa
Kajiru <sdkfz181tiger@gmail.com>
2026-07-15 11:28:56 +0900

    Update

--- 2025/pyxel/18_dungeon/main.py
+++ 2025/pyxel/18_dungeon/main.py
@@ -10,6 +10,9 @@ import random
 import sprite
 
 W, H = 160, 120
+START_X = W / 2
+START_Y = H / 2 - 10
+PLAYER_SPD = 1
 
 # Game
 class Game:
@@ -24,15 +27,20 @@ class Game:
         self.scroll_u = 0
         self.scroll_v = 128
 
+        # プレイヤーを初期化
+        self.player = sprite.PlayerSprite(
+            START_X, START_Y, 0, 72, PLAYER_SPD, self)
+
         pyxel.run(self.update, self.draw) # Pyxel実行
 
     def update(self):
         """ 更新処理 """
-        # タイルマップスクロール
-        if pyxel.btn(pyxel.KEY_RIGHT):
-            self.scroll_u += 1
-        if pyxel.btn(pyxel.KEY_LEFT):
-            self.scroll_u -= 1
+
+        # コントロール
+        self.control()
+
+        # プレイヤーを更新
+        self.player.update()
 
     def draw(self):
         """ 描画処理 """
@@ -41,6 +49,38 @@ class Game:
         # タイルマップ
         # x, y, tm, u, v, w, h, colkey=None, rotate=0, scale=1
         pyxel.bltm(0, 0, 0, self.scroll_u, self.scroll_v, 128, 128)
+
+        # プレイヤーを描画
+        self.player.draw()
+
+    def control(self):
+
+        # タイルマップスクロール
+        if pyxel.btn(pyxel.KEY_A):
+            self.scroll_u += 1
+        if pyxel.btn(pyxel.KEY_D):
+            self.scroll_u -= 1
+
+        # W
+        if pyxel.btnp(pyxel.KEY_W):
+            self.player.move(270)
+        elif pyxel.btnr(pyxel.KEY_W):
+            self.player.stop()
+        # A
+        if pyxel.btnp(pyxel.KEY_A):
+            self.player.move(180)
+        elif pyxel.btnr(pyxel.KEY_A):
+            self.player.stop()
+        # S
+        if pyxel.btnp(pyxel.KEY_S):
+            self.player.move(90)
+        elif pyxel.btnr(pyxel.KEY_S):
+            self.player.stop()
+        # D
+        if pyxel.btnp(pyxel.KEY_D):
+            self.player.move(0)
+        elif pyxel.btnr(pyxel.KEY_D):
+            self.player.stop()
 
 def main():
     """ メイン処理 """
