bd9b4c701e2af4ff6eab343075741a6f645ec32d
Kajiru <sdkfz181tiger@gmail.com>
2026-07-15 21:19:29 +0900

    Update

--- 2025/pyxel/18/main.py
+++ 2025/pyxel/18/main.py
@@ -11,133 +11,115 @@ import sprite
 
 W, H = 160, 120
 
-START_X = W / 2 - 48
-START_Y = H / 2 - 12
+START_X = 70
+START_Y = 90
 
 MODE_TITLE = "title"
 MODE_PLAY = "play"
 MODE_GAME_OVER = "game_over"
 
-TUNNEL_TOTAL = 48
-
 # Game
 class Game:
     def __init__(self):
-        """ コンストラクタ """
+        """ Constructor """
 
-        # スコアを初期化
+        # Score
         self.score = 0
 
-        # ゲームモード
+        # Game Mode
         self.game_mode = MODE_TITLE
 
-        # プレイヤーを初期化
+        # Player
         self.player = sprite.PlayerSprite(START_X, START_Y)
 
-        # ステージを初期化
+        # Reset
         self.reset()
 
-        # Pyxelの起動
-        pyxel.init(W, H, title="Hello, Pyxel!!")
+        # Pyxel
+        pyxel.init(W, H, title="Hello, Pyxel!!", fps=24)
         pyxel.load("my_resource.pyxres")
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        """ 更新処理 """
 
-        # スコアを更新
+        # Score
         self.score = int(self.player.x - START_X)
 
-        # コントロール
-        self.control()
+        # Controll
+        self.controll()
 
-        # プレイ中判定
+        # Game Mode
         if self.game_mode != MODE_PLAY: return
 
-        # プレイヤーを更新
+        # Player
         self.player.update()
 
-        # トンネルを更新
-        for tunnel in self.tunnels:
-            tunnel.update()
-            if(tunnel.intersects(self.player)):
-                self.game_mode = MODE_GAME_OVER
-                break
+        # x Obstacles
+        self.player.collide_obstacles()
 
-        # 落下判定
+        # Game Over
         if H < self.player.y: 
             self.game_mode = MODE_GAME_OVER
 
     def draw(self):
-        """ 描画処理 """
         pyxel.cls(6)
 
-        # カメラ(セット)
+        # Camera(Set)
         pyxel.camera(self.player.x - START_X, 0)
 
-        # タイルマップ
+        # Tilemap
         pyxel.bltm(0, 0, 0, 0, 0, 192, 128, 0)
 
-        # プレイヤーを描画
+        # Player
         self.player.draw()
 
-        # トンネルを描画
-        for tunnel in self.tunnels:
-            tunnel.draw()
+        pyxel.camera() # Camera(Reset)
 
-        pyxel.camera() # カメラ(リセット)
-
-        # メッセージ
+        # Message
         if self.game_mode == MODE_TITLE:
-            msg = "SPACE TO PLAY"
+            msg = "WASD TO PLAY"
             pyxel.text(W/2-len(msg)*2, H/2, msg, 1)
         elif self.game_mode == MODE_GAME_OVER:
             msg = "GAME OVER"
             pyxel.text(W/2-len(msg)*2, H/2, msg, 1)
 
-        # スコアを描画
+        # Score
         pyxel.text(10, 10, 
             "SCORE:{:04}".format(self.score), 1)
 
     def reset(self):
-        """ ステージを初期化 """
 
-        # プレイヤー
+        # Reset
         self.player.x = START_X
         self.player.y = START_Y
 
-        # トンネル
-        self.tunnels = []
-        for i in range(TUNNEL_TOTAL):
-            pad_x = 42
-            pad_y = random.randint(2, 3) * 8
-            x = START_X + pad_x * i + 32
-            y = H / 2 + random.randint(-2, 2) * 8
-            t_top = sprite.TunnelSprite(x, y-pad_y, 10, True)
-            self.tunnels.append(t_top)
-            t_bottom = sprite.TunnelSprite(x, y+pad_y, 10)
-            self.tunnels.append(t_bottom)
+    def controll(self):
 
-    def control(self):
-        """ コントロール """
-        if not pyxel.btnp(pyxel.KEY_SPACE): return
+        # Game Mode
+        if self.game_mode != MODE_PLAY:
+            if not(pyxel.btnp(pyxel.KEY_W) or 
+                pyxel.btnp(pyxel.KEY_A) or 
+                pyxel.btnp(pyxel.KEY_S) or 
+                pyxel.btnp(pyxel.KEY_D)):
+                return
 
-        # Title -> Play
-        if self.game_mode == MODE_TITLE:
-            self.game_mode = MODE_PLAY
+            # Title -> Play
+            if self.game_mode == MODE_TITLE:
+                self.game_mode = MODE_PLAY
 
-        # Game Over -> Title
-        if self.game_mode == MODE_GAME_OVER:
-            self.game_mode = MODE_TITLE
-            # Reset
-            self.reset()
-
-        # ジャンプ
-        if self.game_mode == MODE_PLAY:
-            self.player.jump()
+            # Game Over -> Title
+            if self.game_mode == MODE_GAME_OVER:
+                self.game_mode = MODE_TITLE
+                self.reset() # Reset
+        else:
+            # Player
+            if pyxel.btnp(pyxel.KEY_A):
+                self.player.jumpL()
+            if pyxel.btnp(pyxel.KEY_D):
+                self.player.jumpR()
 
 def main():
-    """ メイン処理 """
+    """ Main """
     Game()
 
 if __name__ == "__main__":
--- 2025/pyxel/18/sprite.py
+++ 2025/pyxel/18/sprite.py
@@ -8,88 +8,132 @@ import pyxel
 import math
 import random
 
+TILE_NONE = 0
+TILE_OBSTACLE = 1
+
+TILE_TYPES = {
+    (0, 7): TILE_OBSTACLE,
+    (1, 7): TILE_OBSTACLE,
+    (2, 7): TILE_OBSTACLE,
+    (3, 7): TILE_OBSTACLE,
+    (4, 7): TILE_OBSTACLE,
+    (0, 8): TILE_OBSTACLE,
+    (1, 8): TILE_OBSTACLE,
+    (2, 8): TILE_OBSTACLE,
+    (3, 8): TILE_OBSTACLE,
+    (4, 8): TILE_OBSTACLE,
+    (4, 2): TILE_OBSTACLE, # Block
+    (5, 2): TILE_OBSTACLE  # Block
+}
+
 class BaseSprite:
 
     def __init__(self, x, y, w=8, h=8):
-        """ コンストラクタ """
+        """ Constructor """
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.vx = 0
         self.vy = 0
+        self.hw = w / 2
+        self.hh = h / 2
 
     def update(self):
-        """ 更新処理 """
         self.x += self.vx
         self.y += self.vy
 
     def draw(self):
-        """ 描画処理(派生クラスで実装) """
         pass
 
     def move(self, spd, deg):
-        """ 移動 """
         rad = deg * math.pi / 180
-        self.vx = spd * math.cos(rad) # x方向の速度
-        self.vy = spd * math.sin(rad) # y方向の速度
+        self.vx = spd * math.cos(rad)
+        self.vy = spd * math.sin(rad)
+
+    def stop(self):
+        self.vx = 0
+        self.vy = 0
 
     def intersects(self, other):
-        """ 矩形同士の当たり判定(AABB) """
         if other.x + other.w < self.x: return False
         if self.x + self.w < other.x: return False
         if other.y + other.h < self.y: return False
         if self.y + self.h < other.y: return False
         return True
 
+    def get_left(self):
+        return (self.x, self.y + self.hh)
+
+    def get_right(self):
+        return (self.x + self.w, self.y + self.hh)
+
+    def get_top(self):
+        return (self.x + self.hw, self.y)
+
+    def get_bottom(self):
+        return (self.x + self.hw, self.y + self.h)
+
+    def get_tile_type(self, x, y):
+        tilemap = pyxel.tilemaps[0]
+        u, v = x // 8, y // 8
+        tile = tilemap.pget(u, v)
+        if not(tile in TILE_TYPES):
+            return None, u, v
+        return TILE_TYPES[tile], u, v
+
+    def collide_obstacles(self):
+        # x Bottom
+        x, y = self.get_bottom()
+        t_type, u, v = self.get_tile_type(x, y)
+        if t_type == TILE_OBSTACLE:
+            self.y = (v-1) * 8
+            self.vx = 0
+            self.vy = 0
+            return
+        # x Top
+        x, y = self.get_top()
+        t_type, u, v = self.get_tile_type(x, y)
+        if t_type == TILE_OBSTACLE:
+            self.y = (v+1) * 8
+            self.vy = 0
+            return
+        # x Left
+        x, y = self.get_left()
+        t_type, u, v = self.get_tile_type(x, y)
+        if t_type == TILE_OBSTACLE:
+            self.x = (u+1) * 8
+            self.vx = 0
+            return
+        # x Right
+        x, y = self.get_right()
+        t_type, u, v = self.get_tile_type(x, y)
+        if t_type == TILE_OBSTACLE:
+            self.x = (u-1) * 8
+            self.vx = 0
+            return
+                
 class PlayerSprite(BaseSprite):
 
     def __init__(self, x, y):
-        """ コンストラクタ """
+        """ Constructor """
         super().__init__(x, y)
-        self.gravity = 0.4 # 重力
-        self.jump_x = 1.0 # ジャンプx
-        self.jump_y = -3.4 # ジャンプy
+        self.gravity = 0.4 # Gravity
+        self.jump_x = 0.8
+        self.jump_y = -2.4
 
     def update(self):
-        """ 更新処理 """
         super().update()
         self.vy += self.gravity # Gravity
 
     def draw(self):
-        """ 描画処理 """
         pyxel.blt(self.x, self.y, 0, 
             0, 16, self.w, self.h, 0)
-        # Debug
-        #pyxel.rectb(self.x, self.y, self.w, self.h, 3)
 
-    def jump(self):
-        """ ジャンプ """
-        self.vx = self.jump_x # ジャンプx
-        self.vy = self.jump_y # ジャンプy
+    def jumpL(self):
+        self.vx = -self.jump_x
+        self.vy = self.jump_y
 
-class TunnelSprite(BaseSprite):
-
-    def __init__(self, x, y, length, top_flg=False):
-        """ コンストラクタ """
-        super().__init__(x, y, 16, length*8)
-        self.length = length # トンネルの長さ
-        if(top_flg): self.y -= self.h # トンネル上
-
-    def draw(self):
-        """ 描画処理 """
-        # Top
-        pyxel.blt(self.x, self.y, 0, 
-            16, 16, self.w, 8, 0)
-        # Mid
-        for i in range(self.length-2):
-            y = self.y + (i+1)*8
-            pyxel.blt(self.x, y, 0, 
-                16, 24, self.w, 8, 0)
-        # Bottom
-        y = self.y + (self.length-1)*8
-        pyxel.blt(self.x, y, 0, 
-            16, 32, self.w, 8, 0)
-        # Debug
-        #pyxel.rectb(self.x, self.y, self.w, self.h, 3)
-
+    def jumpR(self):
+        self.vx = self.jump_x
+        self.vy = self.jump_y
